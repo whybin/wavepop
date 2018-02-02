615d369bad62db08ebeb579e58555e235ceef41b
whybin <31753349+whybin@users.noreply.github.com>
2018-02-02 16:18:01 -0500

    Fix patternizer

--- src/chunker.rs
+++ src/chunker.rs
@@ -21,7 +21,7 @@ fn normalize(data: &Vec<usize>) -> Vec<u8> {
     ret
 }
 
-struct PatternMap {
+pub struct PatternMap {
     /// Array of lengths
     patterns: Vec<u32>,
     /// For each index, an array of pattern indices
@@ -36,7 +36,11 @@ impl PatternMap {
         }
     }
 
-    fn patterns_at(&self, index: usize) -> &Vec<u32> {
+    pub fn pattern_length_at(&self, index: u32) -> u32 {
+        self.patterns[index as usize]
+    }
+
+    pub fn patterns_at(&self, index: usize) -> &Vec<u32> {
         &self.order[index]
     }
 
@@ -54,7 +58,14 @@ impl PatternMap {
 
 /// Items 1..n of `lcp` must contain values greater than 1
 fn add_patterns(pattern_map: &mut PatternMap, indices: &[u32], lcp: &[u32]) {
+    let mut skip_indices: Vec<usize> = vec![];
+
     for i in 1..lcp.len() {
+        if let Some(pos) = skip_indices.iter().position(|&idx| idx == i) {
+            skip_indices.remove(pos);
+            continue;
+        }
+
         let len = lcp[i];
         let pattern_index = pattern_map.new_pattern(len);
 
@@ -62,6 +73,12 @@ fn add_patterns(pattern_map: &mut PatternMap, indices: &[u32], lcp: &[u32]) {
         for j in i..lcp.len() {
             if lcp[j] < len {
                 break;
+            }
+
+            if lcp[j] == len {
+                // Skip an index associated with the same length
+                // e.g. 2, 3, 2, skip the second 2
+                skip_indices.push(j);
             }
 
             pattern_map.use_pattern(indices[j], pattern_index);
@@ -78,6 +95,7 @@ fn add_patterns(pattern_map: &mut PatternMap, indices: &[u32], lcp: &[u32]) {
             pattern_map.use_pattern(indices[j], pattern_index);
         }
 
+        // Add the index prior
         pattern_map.use_pattern(indices[k], pattern_index);
     }
 }
@@ -90,8 +108,9 @@ fn patternize(indices: &[u32], lcp: &[u32]) -> PatternMap {
     loop {
         let mut it = lcp[i..].iter();
         if let Some(start) = it.position(|&prefix_len| prefix_len > 1) {
+            let start = start + i;  // Handle relative position
             let end = match it.position(|&prefix_len| prefix_len <= 1) {
-                Some(idx) => idx,
+                Some(idx) => idx + start + 1,   // Offset as `position` consumes
                 None => lcp.len()
             };
 
@@ -159,10 +178,121 @@ mod tests {
     }
 
     #[test]
+    fn add_patterns_adds_two_items() {
+        let indices = vec![1, 0];
+        let lcp = vec![0, 3];
+        let mut map = PatternMap::new(lcp.len());
+        add_patterns(&mut map, &indices, &lcp);
+        assert_eq!(map.pattern_length_at(0), 3);
+        assert_eq!(map.patterns_at(0), &vec![0]);
+        assert_eq!(map.patterns_at(1), &vec![0]);
+    }
+
+    #[test]
+    fn add_patterns_for_decreasing_lengths() {
+        let indices = vec![2, 1, 0];
+        let lcp = vec![0, 3, 2];
+        let mut map = PatternMap::new(lcp.len());
+        add_patterns(&mut map, &indices, &lcp);
+
+        let expected_lengths: Vec<u32> = vec![3, 2];
+        for (i, &x) in expected_lengths.iter().enumerate() {
+            assert_eq!(map.pattern_length_at(i as u32), x);
+        }
+        let expected_patterns: Vec<Vec<u32>> =
+            vec![vec![1], vec![0, 1], vec![0, 1]];
+        for (i, x) in expected_patterns.iter().enumerate() {
+            assert_eq!(map.patterns_at(i), x);
+        }
+    }
+
+    #[test]
+    fn add_patterns_for_increasing_lengths() {
+        let indices = vec![0, 1, 2];
+        let lcp = vec![0, 2, 3];
+        let mut map = PatternMap::new(lcp.len());
+        add_patterns(&mut map, &indices, &lcp);
+
+        let expected_lengths: Vec<u32> = vec![2, 3];
+        for (i, &x) in expected_lengths.iter().enumerate() {
+            assert_eq!(map.pattern_length_at(i as u32), x);
+        }
+        let expected_patterns: Vec<Vec<u32>> =
+            vec![vec![0], vec![0, 1], vec![0, 1]];
+        for (i, x) in expected_patterns.iter().enumerate() {
+            assert_eq!(map.patterns_at(i), x);
+        }
+    }
+
+    #[test]
+    fn add_patterns_for_same_lengths() {
+        let indices = vec![0, 1, 2];
+        let lcp = vec![0, 2, 2];
+        let mut map = PatternMap::new(lcp.len());
+        add_patterns(&mut map, &indices, &lcp);
+
+        assert_eq!(map.pattern_length_at(0), 2);
+        for i in 0..lcp.len() {
+            assert_eq!(map.patterns_at(i), &vec![0]);
+        }
+    }
+
+    #[test]
+    fn add_patterns_for_varying_lengths() {
+        let indices = vec![0, 1, 2, 3, 4];
+        let lcp = vec![0, 2, 3, 4, 2];
+        let mut map = PatternMap::new(lcp.len());
+        add_patterns(&mut map, &indices, &lcp);
+
+        let expected_lengths: Vec<u32> = vec![2, 3, 4];
+        for (i, &x) in expected_lengths.iter().enumerate() {
+            assert_eq!(map.pattern_length_at(i as u32), x);
+        }
+        let expected_patterns: Vec<Vec<u32>> =
+            vec![vec![0], vec![0, 1], vec![0, 1, 2], vec![0, 1, 2], vec![0]];
+        for (i, x) in expected_patterns.iter().enumerate() {
+            assert_eq!(map.patterns_at(i), x);
+        }
+    }
+
+    #[test]
+    fn add_patterns_test_skip() {
+        let indices = vec![0, 1, 2, 3, 4, 5];
+        let lcp = vec![0, 3, 2, 3, 4, 2];
+        let mut map = PatternMap::new(lcp.len());
+        add_patterns(&mut map, &indices, &lcp);
+
+        let expected_lengths: Vec<u32> = vec![3, 2, 3, 4];
+        for (i, &x) in expected_lengths.iter().enumerate() {
+            assert_eq!(map.pattern_length_at(i as u32), x);
+        }
+
+        let expected_patterns: Vec<Vec<u32>> = vec![
+            vec![0, 1], vec![0, 1], vec![1, 2], vec![1, 2, 3], vec![1, 2, 3],
+            vec![1]
+        ];
+        for (i, x) in expected_patterns.iter().enumerate() {
+            assert_eq!(map.patterns_at(i), x);
+        }
+    }
+
+    #[test]
     fn patternize_one_item_produces_no_patterns() {
         let indices = vec![0];
         let lcp = vec![0];
         let map = patternize(&indices, &lcp);
         assert_eq!(map.patterns_at(0).len(), 0);
+    }
+
+    #[test]
+    fn patternize_multiple_segments() {
+        let indices = vec![0, 1, 2, 3];
+        let lcp = vec![0, 3, 0, 3];
+        let map = patternize(&indices, &lcp);
+
+        let expected_lengths = vec![3, 3];
+        for (i, &x) in expected_lengths.iter().enumerate() {
+            assert_eq!(map.pattern_length_at(i as u32), x);
+        }
     }
 }
