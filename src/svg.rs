d58fe6b297282def9a71997b337df9a8f9791a97
whybin <31753349+whybin@users.noreply.github.com>
2018-02-03 21:06:42 -0500

    Improve SVG output

--- src/svg.rs
+++ src/svg.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use rustsvg::node::element::path::Data;
 use rustsvg::node::element::Path;
 use rustsvg::{Document, save};
@@ -5,8 +7,9 @@ use rustsvg::{Document, save};
 use chunker::PatternMap;
 
 pub fn to_svg(pattern_map: &PatternMap, width: usize, height: usize) {
-    let hor_spacing = width / pattern_map.num_indices();
-    let ver_spacing = height / pattern_map.num_patterns();
+    let hor_spacing = max(1, width / pattern_map.num_indices());
+    let ver_spacing = max(1, height / pattern_map.num_patterns());
+    const HOR_UNITS: u32 = 6;
     
     let mut doc = Document::new()
         .set("viewbox", (0, 0, hor_spacing, ver_spacing));
@@ -17,7 +20,7 @@ pub fn to_svg(pattern_map: &PatternMap, width: usize, height: usize) {
             let data = Data::new()
                 .move_to((i as usize * hor_spacing,
                           pattern_idx as usize * ver_spacing))
-                .line_by((len, 0));
+                .line_by((len * HOR_UNITS, 0));
 
             let path = Path::new()
                 .set("fill", "none")
@@ -29,5 +32,5 @@ pub fn to_svg(pattern_map: &PatternMap, width: usize, height: usize) {
         }
     }
 
-    save("test.svg", &doc).unwrap();
+    save("assets/images/output.svg", &doc).unwrap();
 }
